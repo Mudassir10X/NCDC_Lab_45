f41ee51619e8013c90b3a58675e25b158bb724fd
Mudassir Ali <smudassirali1@gmail.com>
2025-07-01 14:59:07 +0500

    Changed task1

--- task1_mcseq/tb/file.f
+++ task1_mcseq/tb/file.f
@@ -6,8 +6,8 @@
 // include directories
 //*** add incdir include directories here
 -incdir ../router_rtl
--incdir ../task1_integ/sv/
--incdir ../task1_integ/tb/
+-incdir ../task1_mcseq/sv/
+-incdir ../task1_mcseq/tb/
 -incdir ../channel/sv
 -incdir ../clock_and_reset/sv
 -incdir ../hbus/sv
@@ -20,10 +20,10 @@
 ../clock_and_reset/sv/clock_and_reset_if.sv
 ../hbus/sv/hbus_pkg.sv
 ../hbus/sv/hbus_if.sv
-../task1_integ/sv/yapp_pkg.sv
-../task1_integ/sv/yapp_if.sv
-../task1_integ/tb/clkgen.sv
-../task1_integ/tb/top.sv
+../task1_mcseq/sv/yapp_pkg.sv
+../task1_mcseq/sv/yapp_if.sv
+../task1_mcseq/tb/clkgen.sv
+../task1_mcseq/tb/top.sv
 ../router_rtl/yapp_router.sv
-../task1_integ/tb/hw_top.sv
+../task1_mcseq/tb/hw_top.sv
 
